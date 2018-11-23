8d053a64bc3da209afc971ccd1daab879d432c0c
May the source be with you <misa.zhu@gmail.com>
2018-11-24 04:44:04 +0800

    reorg path

--- basic/regex/build.mk
+++
@@ -1,9 +0,0 @@
-REGEX_OBJS= $(mario_OBJS) $(mario_js_OBJS) \
-	$(REGEX)/native_regex.o 
-
-CXXFLAGS += -std=c++11
-
-$(REGEX_TARGET): $(REGEX_OBJS)
-	$(LD) -shared -o $(REGEX_TARGET) $(REGEX_OBJS) $(LDFLAGS)
-
-CLEAN += $(REGEX_TARGET) $(REGEX_OBJS)
